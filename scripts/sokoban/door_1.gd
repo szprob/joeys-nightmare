8277049fc9130ddb255c2679cc0f6451e3a904a3
zypan0 <panziyi1@126.com>
2024-10-24 16:29:34 +0800

    [fix] door script

---
+++ scripts/sokoban/door_1.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func open():
+	print('open door')
+	self.set_collision_layer(0)
+	
+func close():
+	print('close door')
+	self.set_collision_layer(1)
