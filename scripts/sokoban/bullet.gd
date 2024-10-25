698340ee253acf5673e03c102fd0ca79eca86d57
ziyi.pan <panziyi1@126.com>
2024-10-25 15:41:33 +0800

    [feat] bullet can place a gravity area

--- scripts/sokoban/bullet.gd
+++ scripts/sokoban/bullet.gd
@@ -31,12 +31,18 @@ func _process(delta: float) -> void:
 
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	# TODO: hit tilemap and put a gravity
 	if body is TileMapLayer:
 		print('hit tilemap, add gravity')
-		queue_free()
-		
-	
+		# 实例化gravity节点
+		var gravity_scene = preload("res://scenes/sokoban/gravity_1.tscn")
+		var gravity_instance = gravity_scene.instantiate()
 
+		# 设置gravity节点的位置为当前子弹的位置
+		gravity_instance.position = position + Vector2(0, gravity_instance.get_node("CollisionShape2D").shape.get_size().y/2)
+		
+		get_tree().root.add_child(gravity_instance)
+		queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
