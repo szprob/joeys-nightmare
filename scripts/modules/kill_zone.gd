dc1d725552341e538cb001f7e6a0b169d6b150f5
ziyi.pan <panziyi1@126.com>
2024-10-27 22:57:42 +0800

    [fix] reload when died

--- scripts/modules/kill_zone.gd
+++ scripts/modules/kill_zone.gd
@@ -11,4 +11,16 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
-	get_tree().reload_current_scene()
+	# var root_node = get_tree().get_root()
+	var root_node = get_tree().current_scene
+	print('root node', root_node.name)
+	
+	cleanup_dynamic_nodes()
+	get_tree().change_scene_to_file(root_node.scene_file_path)
+
+
+func cleanup_dynamic_nodes() -> void:
+	# 假设你给所有动态添加的节点都加了一个组名
+	var dynamic_nodes = get_tree().get_nodes_in_group("dynamic")
+	for node in dynamic_nodes:
+		node.queue_free()
