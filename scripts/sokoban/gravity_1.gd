65a5c6c1c5de403503b1bd6cd53e32161ef123be
whozawhat <whozawhat@163.com>
2024-10-27 21:55:04 +0800

    conflicts resolve

--- scripts/sokoban/gravity_1.gd
+++ scripts/sokoban/gravity_1.gd
@@ -2,7 +2,7 @@
 extends Area2D
 
 var debug_draw_enabled = true
-@export var arrow_length := 50.0
+@export var arrow_length := 16.0
 @export var arrow_color := Color.YELLOW
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,10 +23,10 @@ func _draw():
 	# 只在重力覆盖启用时绘制
 	if gravity_space_override != SPACE_OVERRIDE_DISABLED:
 		# 绘制主线
-		draw_line(Vector2.ZERO, end_point, arrow_color, 2.0)
+		draw_line(Vector2.ZERO-end_point, end_point, arrow_color, 2.0)
 		
 		# 绘制箭头头部
-		var arrow_head_length = arrow_length * 0.2
+		var arrow_head_length = arrow_length * 0.4
 		var arrow_angle = normalized_direction.angle()
 		var head_point1 = end_point + Vector2.from_angle(arrow_angle + PI * 0.8) * arrow_head_length
 		var head_point2 = end_point + Vector2.from_angle(arrow_angle - PI * 0.8) * arrow_head_length
