77b904f498e8bc2e69e1f4c2cc5fe44d7f8cc8eb
ze.song <ze.song@garena.com>
2024-10-29 19:48:03 +0800

    a

--- scripts/modules/platforms/disapper_platform.gd
+++ scripts/modules/platforms/disapper_platform.gd
@@ -1,20 +1,55 @@
 extends AnimatableBody2D
 
-var is_visible = true
 
-@onready var timer: Timer = $Timer
+var do_detect = true 
+
+@export var init_time_delay: float = 1
+@export var blink_time: float = 3
+@export var blink_gap: float = 0.5
+
+
+@onready var timer: Timer = Timer.new()
+@onready var timer_2: Timer = Timer.new()
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animation_player: AnimationPlayer = AnimationPlayer.new()
 
-
+func _ready() -> void:
+	timer.wait_time = init_time_delay
+	timer_2.wait_time = blink_time
+	add_child(timer)
+	add_child(timer_2)
+	add_child(animation_player)
+	timer.timeout.connect(_on_timer_timeout)
+	timer_2.timeout.connect(_on_timer_2_timeout)
 
 func _on_detection_body_entered(body: Node2D) -> void:
-	if is_visible:
+	if do_detect:
 		if body is CharacterBody2D:
+			do_detect=false
 			timer.start()
 
 
 func _on_timer_timeout() -> void:
-	is_visible = !is_visible
-	sprite_2d.visible = is_visible
-	collision_shape_2d.disabled = !is_visible
+	timer_2.start()
+	start_blinking()
+
+
+func _on_timer_2_timeout() -> void:
+	queue_free()
+
+func start_blinking() -> void:
+	var blink_animation = Animation.new()
+	blink_animation.length = blink_time
+	blink_animation.set_loop_mode(1)
+
+	var track_index = blink_animation.add_track(Animation.TYPE_VALUE)
+	blink_animation.track_set_path(track_index, "Sprite2D:visible")
+	
+	for i in range(0, int(blink_time * 2)):
+		var time = i * 0.5
+		var value = i % 2 == 0
+		blink_animation.track_insert_key(track_index, time, value)
+
+	animation_player.add_animation("blink", blink_animation)
+	animation_player.play("blink")
--- scripts/modules/platforms/move_platform.gd
+++ scripts/modules/platforms/move_platform.gd
@@ -4,15 +4,14 @@ var is_moving: bool = false
 var move_direction: Vector2 = Vector2.ZERO
 var distance  = 0 
 var final_position : Vector2 = Vector2.ZERO
+var still_there = false
 var do_detect = true #是否做检测
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var timer: Timer = $Timer
 @onready var target: Area2D = $target
-@onready var target_sprite: Sprite2D = $target/Sprite2D2
 
 func _on_ready() -> void:
-	target_sprite.visible = false
 	final_position=target.global_position
 	move_direction = (target.global_position - position).normalized()
 	distance = final_position.distance_to(position)
@@ -20,6 +19,7 @@ func _on_ready() -> void:
 func _on_detection_body_entered(body: Node2D) -> void:
 	if do_detect and body is CharacterBody2D:
 		do_detect=false
+		still_there = true
 		timer.start()
 
 func _physics_process(delta):
@@ -30,4 +30,10 @@ func _physics_process(delta):
 			is_moving = false
 
 func _on_timer_timeout() -> void:
-	is_moving = true
+	if still_there:
+		is_moving = true
+
+
+func _on_detection_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		still_there = false
