fbd0d81dcfd3112bc9cca3a1be924546744b93ee
ze.song <ze.song@garena.com>
2024-10-30 14:43:54 +0800

    a

--- scripts/modules/platforms/disapper_platform.gd
+++ scripts/modules/platforms/disapper_platform.gd
@@ -17,6 +17,8 @@ var do_detect = true
 func _ready() -> void:
 	timer.wait_time = init_time_delay
 	timer_2.wait_time = blink_time
+	timer.one_shot = true
+	timer_2.one_shot = true
 	add_child(timer)
 	add_child(timer_2)
 	add_child(animation_player)
@@ -33,23 +35,29 @@ func _on_detection_body_entered(body: Node2D) -> void:
 func _on_timer_timeout() -> void:
 	timer_2.start()
 	start_blinking()
+	print("开始闪烁")
+
 
 
 func _on_timer_2_timeout() -> void:
+	print("准备消失")
+	animation_player.stop()
+	sprite_2d.modulate.a = 0
+	collision_shape_2d.set_deferred("disabled", true)
 	queue_free()
 
 func start_blinking() -> void:
-	var blink_animation = Animation.new()
-	blink_animation.length = blink_time
-	blink_animation.set_loop_mode(1)
-
-	var track_index = blink_animation.add_track(Animation.TYPE_VALUE)
-	blink_animation.track_set_path(track_index, "Sprite2D:visible")
+	var animation = Animation.new()
+	var track_index = animation.add_track(Animation.TYPE_VALUE)
+	animation.track_set_path(track_index, "Sprite2D:modulate")
+	animation.length = blink_gap
 	
-	for i in range(0, int(blink_time * 2)):
-		var time = i * 0.5
-		var value = i % 2 == 0
-		blink_animation.track_insert_key(track_index, time, value)
-
-	animation_player.add_animation("blink", blink_animation)
+	animation.track_insert_key(track_index, 0, Color(1, 1, 1, 1))
+	animation.track_insert_key(track_index, blink_gap/2, Color(1, 1, 1, 0))
+	animation.track_insert_key(track_index, blink_gap, Color(1, 1, 1, 1))
+	
+	animation.loop_mode = Animation.LOOP_LINEAR
+	
+	animation_player.add_animation_library("", AnimationLibrary.new())
+	animation_player.get_animation_library("").add_animation("blink", animation)
 	animation_player.play("blink")
