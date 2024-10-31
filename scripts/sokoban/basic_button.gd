6183885e2f483d7f338acacd1ffdad0f2ac3a329
ze.song <ze.song@garena.com>
2024-10-31 18:17:31 +0800

    button

--- scripts/sokoban/basic_button.gd
+++ scripts/sokoban/basic_button.gd
@@ -1,28 +1,36 @@
 extends Area2D
 
-#@export door
 @export var doors: Array[NodePath]
 
+var active_bodies: int = 0
+# 使用字典跟踪已经在按钮上的物体
+var tracked_bodies: Dictionary = {}
+
 func _ready() -> void:
-	# 使用 Callable 连接信号
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
 	
 func _on_body_entered(body):
-	print(body)
-	if body is CharacterBody2D:
-		print('CharacterBody2D entered, open the doors')
-		$AnimatedSprite2D.frame = 1
-		# 遍历所有门并打开
-		for door_path in doors:
-			if door_path:
-				get_node(door_path).open()
+	# 检查是否是玩家或箱子，且还未被记录
+	if (body is CharacterBody2D or body.is_in_group("box")) and not tracked_bodies.has(body):
+		tracked_bodies[body] = true
+		active_bodies += 1
+		if active_bodies >= 1:
+			print('Button activated, open the doors')
+			$AnimatedSprite2D.frame = 1
+			for door_path in doors:
+				if door_path:
+					get_node(door_path).open()
 		
 func _on_body_exited(body):
-	if body is CharacterBody2D:
-		print('CharacterBody2D exited, close the doors')
-		$AnimatedSprite2D.frame = 0
-		# 遍历所有门并关闭
-		for door_path in doors:
-			if door_path:
-				get_node(door_path).close()
+	# 检查是否是已记录的玩家或箱子
+	if tracked_bodies.has(body):
+		tracked_bodies.erase(body)
+		active_bodies -= 1
+		if active_bodies <= 0:
+			active_bodies = 0
+			print('All bodies exited, close the doors')
+			$AnimatedSprite2D.frame = 0
+			for door_path in doors:
+				if door_path:
+					get_node(door_path).close()
