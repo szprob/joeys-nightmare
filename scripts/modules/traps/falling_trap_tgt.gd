27f609ac28e36b2f7d8f7ed00bcfdb153bd440a0
ze.song <ze.song@garena.com>
2024-11-01 18:50:44 +0800

    teleport bug $ falling traps

--- scripts/modules/traps/falling_trap_tgt.gd
+++ scripts/modules/traps/falling_trap_tgt.gd
@@ -1,47 +1,35 @@
 extends Area2D
 
 @export var speed: float = Consts.FALLING_TRAP_SPEED  # 可配置的速度
-@export var reset_delay : float = 1 
-@export var do_reset :bool = false
 @export var invisible_before_launch :bool = true  # 添加是否隐身的配置
+@export var target: Area2D
+@export var detection: Area2D
 
 var is_flying_forward: bool = false
-var is_flying_backward: bool = false
 var fly_direction: Vector2 = Vector2.ZERO
 var init_position: Vector2 = Vector2.ZERO
 var final_position : Vector2 = Vector2.ZERO
+var do_detect = true 
 
 @onready var falling_trap: Area2D = $"."
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var timer: Timer = $Timer
-@onready var target: Area2D = $target
-@onready var target_sprite_2d: Sprite2D = $target/Sprite2D
-@onready var timer2: Timer = $Timer2
-
+@onready var animated_sprited_2d:  AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	init_position = position
-	target_sprite_2d.visible = false
-	final_position=target.global_position
-	timer2.wait_time = reset_delay
+	final_position = target.global_position
 	if invisible_before_launch:  # 根据配置设置初始可见性
 		modulate.a = 0  # 完全透明
-
+	# 添加 detection 区域的信号连接
+	detection.body_entered.connect(_on_detection_body_entered)
+	# 添加默认动画播放
+	animated_sprited_2d.play("default")
 
 func _physics_process(delta):
 	if is_flying_forward:
 		position += fly_direction * speed * delta
-		if position.distance_to(final_position) < speed * delta:  # 10.0 is a small threshold to consider it reached
+		if position.distance_to(final_position) < 5:
 			position = final_position
-			if do_reset:
-				is_flying_forward=false
-				timer2.start() 
-			else:
-				queue_free()
-	if is_flying_backward:
-		position += fly_direction * speed * delta
-		if position.distance_to(init_position) < speed * delta:  # 10.0 is a small threshold to consider it reached
-			position = init_position
 			queue_free()
 		
 		
@@ -52,10 +40,10 @@ func _on_timer_timeout() -> void:
 		modulate.a = 1  # 发射时显示
 	fly_direction = (final_position  - position).normalized()
 
-func _on_body_entered(body: Node2D) -> void:
-	if body is CharacterBody2D : 
-		timer.start()
 
-func _on_timer_2_timeout() -> void:
-	is_flying_backward = true
-	fly_direction = (init_position  - position).normalized()
+
+# 添加新的检测函数
+func _on_detection_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and do_detect:
+		timer.start()
+		do_detect=false
