aa33ed8f801a63f48570f8e432f723f0ade77e42
ze.song <ze.song@garena.com>
2024-12-03 14:06:53 +0800

    swing

--- scripts/modules/traps/fires.gd
+++ scripts/modules/traps/fires.gd
@@ -2,8 +2,12 @@ extends AnimatableBody2D
 
 @export var rotation_angular_velocity: float = 5.0
 @export var clockwise: bool = true
+@export var initial_rotation: float = 0.0
+
+func _ready():
+	rotation = initial_rotation
 
 func _physics_process(delta):
-    var rotation_direction = 1 if clockwise else -1
-    rotation += rotation_angular_velocity * delta * rotation_direction
-
+	var rotation_direction = 1 if clockwise else -1
+	rotation += rotation_angular_velocity * delta * rotation_direction
+	
