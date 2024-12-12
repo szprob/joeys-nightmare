b0c8a758d2e8415a59a5c2cb3226af866f922d0d
whozawhat <whozawhat@163.com>
2024-12-12 21:03:29 +0800

    intro assets fix

--- scenes/dreams/bigworldv2/chase_real.gd
+++ scenes/dreams/bigworldv2/chase_real.gd
@@ -3,12 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-<<<<<<< Updated upstream
 	if not GameManager.game_state_cache['bmg_set']:
 		GameManager.setup_bgm_player()
 	GameManager.play_bgm('chase')
 	GameManager.game_state_cache['should_die'] = true
 	
-=======
-	GameManager.game_state_cache['should_die'] = true
->>>>>>> Stashed changes
