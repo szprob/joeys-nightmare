9cef40910783bc68dcc6198f1cc4502ba363efc7
ze.song <ze.song@garena.com>
2024-12-12 18:57:10 +0800

    bgm

---
+++ scenes/dreams/bigworldv2/cave.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not GameManager.game_state_cache['bmg_set']:
+		GameManager.setup_bgm_player()
+	GameManager.play_bgm('cave')
