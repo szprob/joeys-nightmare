6d66098687c32a27acf0ea5fa83397c1099273b0
Selyx-LT <56570807+Selyx-LT@users.noreply.github.com>
2024-12-16 00:31:12 +0800

    day finish (#263)
    Co-authored-by: Selyx <selyx.huang@SelyxdeMac-mini.local>

--- scripts/day/message.gd
+++ scripts/day/message.gd
@@ -3,11 +3,11 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if GameManager.has_item('信'):
+	if GameManager.get_day_phase() != 10:
 		queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if GameManager.has_item('信'):
+	if GameManager.has_item('留言'):
 		queue_free()
