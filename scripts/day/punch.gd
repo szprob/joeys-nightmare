de556f89827e6175139781af98fdd8a198175be8
Selyx <selyx.huang@SelyxdeMac-mini.local>
2024-12-29 00:00:53 +0800

    add mini game linkage

---
+++ scripts/day/punch.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export var animation: AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func action() -> void:
+	SoundPlayer.play_sound("res://assets/sounds/day/punch_sound.mp3")
+	animation.play("punched")
